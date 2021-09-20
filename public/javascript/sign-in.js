c867a6bbf235505115c6cb723f1f80f764c23c5e
AshisPatel <ashis.n.patel@gmail.com>
2021-09-20 10:21:29 -0500

    Add sign-in functionality

---
+++ public/javascript/sign-in.js
@@ -0,0 +1,32 @@
+
+const signinFormHandler = async function (event) {
+    event.preventDefault();
+    // Get username and password from form
+    const username = document.querySelector('#username-sign-in').value.trim();
+    const password = document.querySelector('#password-sign-in').value.trim();
+
+    // Check to see if there were values provided
+    if (username && password) {
+        // Call the post request in the api for user-routes to sign-in
+        const response = await fetch('/api/users/signin', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-type': 'application/json' }
+        });
+
+        // Check if response went through
+        if (response.ok) {
+            console.log('Signed in!');
+            window.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+
+};
+
+// Listen for submit on sign-up form
+document.querySelector('form').addEventListener('submit', signinFormHandler);
