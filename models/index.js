ef803f521defcc19affa2b90b2afcfa056377674
AshisPatel <ashis.n.patel@gmail.com>
2021-09-24 15:05:06 -0500

    Change associations to cascade

--- models/index.js
+++ models/index.js
@@ -7,33 +7,29 @@ const Comment = require('./Comment');
 // A Post can only belong to one User, but a User can have many Posts
 Post.belongsTo(User, {
     foreignKey: 'user_id',
-    onDelete: 'SET NULL'
+    onDelete: 'CASCADE'
 });
 
 User.hasMany(Post, {
     foreignKey: 'user_id',
-    onDelete: 'SET NULL'
 });
 // A Comment can only belong to one User, but a User can have many Comments
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
-    onDelete: 'SET NULL'
+    onDelete: 'CASCADE'
 });
 
 User.hasMany(Comment, {
     foreignKey: 'user_id',
-    onDelete: 'SET NULL'
 });
 // A Comment can only belong to one Post, but a Post can have many Comments 
 Comment.belongsTo(Post, {
     foreignKey: 'post_id',
-    onDelete: 'SET NULL'
-    
+    onDelete: 'CASCADE'
 });
 
 Post.hasMany(Comment, {
     foreignKey: 'post_id',
-    onDelete: 'SET NULL'
 });
 
 module.exports = { User, Post, Comment };
